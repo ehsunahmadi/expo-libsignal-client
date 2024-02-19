eed577903277787b29cca4fe773a24d574dfeb7d
ehsun <ehsun.ahmadi@gmail.com>
2024-02-20 01:33:03 +0330

    new version with a ton of modules, all working (not tested)

--- android/src/main/java/expo/modules/libsignalclient/ExpoLibsignalClientModule.kt
+++ android/src/main/java/expo/modules/libsignalclient/ExpoLibsignalClientModule.kt
@@ -23,9 +23,62 @@ class ExpoLibsignalClientModule : Module() {
     Function("PublicKey_GetPublicKeyBytes", this@ExpoLibsignalClientModule::PublicKey_GetPublicKeyBytes)
     Function("PublicKey_Verify", this@ExpoLibsignalClientModule::PublicKey_Verify)
     Function("IdentityKey_VerifyAlternateIdentity", this@ExpoLibsignalClientModule::IdentityKey_VerifyAlternateIdentity)
-  }
 
-  private fun HKDF_DeriveSecrets(outputLength: Int, ikm: ByteArray, label: ByteArray | null, salt: ByteArray | null) : ByteArray {
+    Function("KyberPublicKey_Deserialize", this@ExpoLibsignalClientModule::KyberPublicKey_Deserialize)
+    Function("KyberPublicKey_Serialize", this@ExpoLibsignalClientModule::KyberPublicKey_Serialize)
+    Function("KyberSecretKey_Deserialize", this@ExpoLibsignalClientModule::KyberSecretKey_Deserialize)
+    Function("KyberSecretKey_Serialize", this@ExpoLibsignalClientModule::KyberSecretKey_Serialize)
+
+    Function("KyberKeyPair_Generate", this@ExpoLibsignalClientModule::KyberKeyPair_Generate)
+    Function("KyberKeyPair_GetPublicKey", this@ExpoLibsignalClientModule::KyberKeyPair_GetPublicKey)
+    Function("KyberKeyPair_GetSecretKey", this@ExpoLibsignalClientModule::KyberKeyPair_GetSecretKey)
+
+    Function("IdentityKeyPair_Deserialize", this@ExpoLibsignalClientModule::IdentityKeyPair_Deserialize)
+    Function("IdentityKeyPair_Serialize", this@ExpoLibsignalClientModule::IdentityKeyPair_Serialize)
+    Function("IdentityKeyPair_SignAlternateIdentity", this@ExpoLibsignalClientModule::IdentityKeyPair_SignAlternateIdentity)
+
+    // we are passing some arguments in array and receiving them as pairs for reducing the number of parameters to >= 8. we can clean it up further by putting it in a Record class whih is expo's max limit due to the limitations of generics in both Swift and Kotlin because this component must be implemented separately for each.
+    Function("PreKeyBundle_New", this@ExpoLibsignalClientModule::PreKeyBundle_New)
+    Function("PreKeyBundle_GetDeviceId", this@ExpoLibsignalClientModule::PreKeyBundle_GetDeviceId)
+    Function("PreKeyBundle_GetIdentityKey", this@ExpoLibsignalClientModule::PreKeyBundle_GetIdentityKey)
+    Function("PreKeyBundle_GetPreKeyId", this@ExpoLibsignalClientModule::PreKeyBundle_GetPreKeyId)
+    Function("PreKeyBundle_GetPreKeyPublic", this@ExpoLibsignalClientModule::PreKeyBundle_GetPreKeyPublic)
+    Function("PreKeyBundle_GetRegistrationId", this@ExpoLibsignalClientModule::PreKeyBundle_GetRegistrationId)
+    Function("PreKeyBundle_GetSignedPreKeyId", this@ExpoLibsignalClientModule::PreKeyBundle_GetSignedPreKeyId)
+    Function("PreKeyBundle_GetSignedPreKeyPublic", this@ExpoLibsignalClientModule::PreKeyBundle_GetSignedPreKeyPublic)
+    Function("PreKeyBundle_GetSignedPreKeySignature", this@ExpoLibsignalClientModule::PreKeyBundle_GetSignedPreKeySignature)
+    Function("PreKeyBundle_GetKyberPreKeyId", this@ExpoLibsignalClientModule::PreKeyBundle_GetKyberPreKeyId)
+    Function("PreKeyBundle_GetKyberPreKeyPublic", this@ExpoLibsignalClientModule::PreKeyBundle_GetKyberPreKeyPublic)
+    Function("PreKeyBundle_GetKyberPreKeySignature", this@ExpoLibsignalClientModule::PreKeyBundle_GetKyberPreKeySignature)
+
+    Function("PreKeyRecord_New", this@ExpoLibsignalClientModule::PreKeyRecord_New)
+    Function("PreKeyRecord_Deserialize", this@ExpoLibsignalClientModule::PreKeyRecord_Deserialize)
+    Function("PreKeyRecord_GetId", this@ExpoLibsignalClientModule::PreKeyRecord_GetId)
+    Function("PreKeyRecord_GetPrivateKey", this@ExpoLibsignalClientModule::PreKeyRecord_GetPrivateKey)
+    Function("PreKeyRecord_GetPublicKey", this@ExpoLibsignalClientModule::PreKeyRecord_GetPublicKey)
+    Function("PreKeyRecord_Serialize", this@ExpoLibsignalClientModule::PreKeyRecord_Serialize)
+
+    Function("SignedPreKeyRecord_New", this@ExpoLibsignalClientModule::SignedPreKeyRecord_New)
+    Function("SignedPreKeyRecord_Deserialize", this@ExpoLibsignalClientModule::SignedPreKeyRecord_Deserialize)
+    Function("SignedPreKeyRecord_GetId", this@ExpoLibsignalClientModule::SignedPreKeyRecord_GetId)
+    Function("SignedPreKeyRecord_GetPrivateKey", this@ExpoLibsignalClientModule::SignedPreKeyRecord_GetPrivateKey)
+    Function("SignedPreKeyRecord_GetPublicKey", this@ExpoLibsignalClientModule::SignedPreKeyRecord_GetPublicKey)
+    Function("SignedPreKeyRecord_Serialize", this@ExpoLibsignalClientModule::SignedPreKeyRecord_Serialize)
+    Function("SignedPreKeyRecord_GetSignature", this@ExpoLibsignalClientModule::SignedPreKeyRecord_GetSignature)
+    Function("SignedPreKeyRecord_GetTimestamp", this@ExpoLibsignalClientModule::SignedPreKeyRecord_GetTimestamp)
+
+    Function("KyberPreKeyRecord_New", this@ExpoLibsignalClientModule::KyberPreKeyRecord_New)
+    Function("KyberPreKeyRecord_Serialize", this@ExpoLibsignalClientModule::KyberPreKeyRecord_Serialize)
+    Function("KyberPreKeyRecord_Deserialize", this@ExpoLibsignalClientModule::KyberPreKeyRecord_Deserialize)
+    Function("KyberPreKeyRecord_GetId", this@ExpoLibsignalClientModule::KyberPreKeyRecord_GetId)
+    Function("KyberPreKeyRecord_GetKeyPair", this@ExpoLibsignalClientModule::KyberPreKeyRecord_GetKeyPair)
+    Function("KyberPreKeyRecord_GetPublicKey", this@ExpoLibsignalClientModule::KyberPreKeyRecord_GetPublicKey)
+    Function("KyberPreKeyRecord_GetSecretKey", this@ExpoLibsignalClientModule::KyberPreKeyRecord_GetSecretKey)
+    Function("KyberPreKeyRecord_GetSignature", this@ExpoLibsignalClientModule::KyberPreKeyRecord_GetSignature)
+    Function("KyberPreKeyRecord_GetTimestamp", this@ExpoLibsignalClientModule::KyberPreKeyRecord_GetTimestamp)
+  }
+  
+  private fun HKDF_DeriveSecrets(outputLength: Int, ikm: ByteArray, label: ByteArray?, salt: ByteArray?) : ByteArray {
     return Native.HKDF_DeriveSecrets(outputLength, ikm, label, salt)
   }
 
@@ -65,5 +118,174 @@ class ExpoLibsignalClientModule : Module() {
   }
   private fun IdentityKey_VerifyAlternateIdentity(identityKey: Long, publicKey: Long, signature: ByteArray) : Boolean {
     return Native.IdentityKey_VerifyAlternateIdentity(identityKey, publicKey, signature)
+  }
+
+  private fun KyberPublicKey_Deserialize(serialized: ByteArray) : Long {
+    return Native.KyberPublicKey_DeserializeWithOffset(serialized, 0)
+  }
+  private fun KyberPublicKey_Serialize(publicKey: Long) : ByteArray {
+    return Native.KyberPublicKey_Serialize(publicKey)
+  }
+  private fun KyberSecretKey_Deserialize(serialized: ByteArray) : Long {
+    return Native.KyberSecretKey_Deserialize(serialized)
+  }
+  private fun KyberSecretKey_Serialize(secretKey: Long) : ByteArray {
+    return Native.KyberSecretKey_Serialize(secretKey)
+  }
+
+  private fun KyberKeyPair_Generate() : Long {
+    return Native.KyberKeyPair_Generate()
+  }
+  private fun KyberKeyPair_GetPublicKey(keyPair: Long) : Long {
+    return Native.KyberKeyPair_GetPublicKey(keyPair)
+  }
+  private fun KyberKeyPair_GetSecretKey(keyPair: Long) : Long {
+    return Native.KyberKeyPair_GetSecretKey(keyPair)
+  }
+
+  private fun IdentityKeyPair_Deserialize(serialized: ByteArray) : LongArray {
+    return Native.IdentityKeyPair_Deserialize(serialized)
+  }
+
+  private fun IdentityKeyPair_Serialize(publicKey: Long, privateKey: Long) : ByteArray {
+    return Native.IdentityKeyPair_Serialize(publicKey, privateKey)
+  }
+  private fun IdentityKeyPair_SignAlternateIdentity(publicKey: Long, privateKey: Long, otherPublicKey: Long) : ByteArray {
+    return Native.IdentityKeyPair_SignAlternateIdentity(publicKey, privateKey, otherPublicKey)
+  }
+
+    // we are passing some arguments in array and receiving them as pairs for reducing the number of parameters to >= 8. we can clean it up further by putting it in a Record class whih is expo's max limit due to the limitations of generics in both Swift and Kotlin because this component must be implemented separately for each.
+  private fun PreKeyBundle_New(
+    registerationData: kotlin.Pair<Int, Int>,
+    preKeyId: Int,
+    preKeyPublic: Long,
+    signedPreKeyId: Int,
+    signedPreKeyData: kotlin.Pair< Long, ByteArray>,
+    identityKey: Long,
+    kyberPreKeyId: Int,
+    kyberPreKeyData: kotlin.Pair< Long, ByteArray>
+  ) : Long {
+    var (registrationId, deviceId) = registerationData
+    var (signedPreKeyPublic, signedPreKeySignature) = signedPreKeyData
+    var (kyberPreKeyPublic, kyberPreKeySignature) = kyberPreKeyData
+
+    return Native.PreKeyBundle_New(
+      registrationId,
+      deviceId,
+      preKeyId,
+      preKeyPublic,
+      signedPreKeyId,
+      signedPreKeyPublic,
+      signedPreKeySignature,
+      identityKey,
+      kyberPreKeyId,
+      kyberPreKeyPublic,
+      kyberPreKeySignature
+    )
+  }
+  private fun PreKeyBundle_GetDeviceId(preKeyBundle: Long) : Int {
+    return Native.PreKeyBundle_GetDeviceId(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetIdentityKey(preKeyBundle: Long) : Long {
+    return Native.PreKeyBundle_GetIdentityKey(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetPreKeyId(preKeyBundle: Long) : Int {
+    return Native.PreKeyBundle_GetPreKeyId(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetPreKeyPublic(preKeyBundle: Long) : Long {
+    return Native.PreKeyBundle_GetPreKeyPublic(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetRegistrationId(preKeyBundle: Long) : Int {
+    return Native.PreKeyBundle_GetRegistrationId(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetSignedPreKeyId(preKeyBundle: Long) : Int {
+    return Native.PreKeyBundle_GetSignedPreKeyId(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetSignedPreKeyPublic(preKeyBundle: Long) : Long {
+    return Native.PreKeyBundle_GetSignedPreKeyPublic(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetSignedPreKeySignature(preKeyBundle: Long) : ByteArray {
+    return Native.PreKeyBundle_GetSignedPreKeySignature(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetKyberPreKeyId(preKeyBundle: Long) : Int {
+    return Native.PreKeyBundle_GetKyberPreKeyId(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetKyberPreKeyPublic(preKeyBundle: Long) : Long {
+    return Native.PreKeyBundle_GetKyberPreKeyPublic(preKeyBundle)
+  }
+  private fun PreKeyBundle_GetKyberPreKeySignature(preKeyBundle: Long) : ByteArray {
+    return Native.PreKeyBundle_GetKyberPreKeySignature(preKeyBundle)
+  }
+
+  private fun PreKeyRecord_New(id: Int, pubKey: Long, privKey: Long) : Long {
+    return Native.PreKeyRecord_New(id, pubKey, privKey)
+  }
+  private fun PreKeyRecord_Deserialize(serialized: ByteArray) : Long {
+    return Native.PreKeyRecord_Deserialize(serialized)
+  }
+  private fun PreKeyRecord_GetId(preKeyRecord: Long) : Int {
+    return Native.PreKeyRecord_GetId(preKeyRecord)
+  }
+  private fun PreKeyRecord_GetPrivateKey(preKeyRecord: Long) : Long {
+    return Native.PreKeyRecord_GetPrivateKey(preKeyRecord)
+  }
+  private fun PreKeyRecord_GetPublicKey(preKeyRecord: Long) : Long {
+    return Native.PreKeyRecord_GetPublicKey(preKeyRecord)
+  }
+  private fun PreKeyRecord_Serialize(preKeyRecord: Long) : ByteArray {
+    return Native.PreKeyRecord_GetSerialized(preKeyRecord)
+  }
+
+  private fun SignedPreKeyRecord_New(id: Int, timestamp: Long, pubKey: Long, privKey: Long, signature: ByteArray) : Long {
+    return Native.SignedPreKeyRecord_New(id, timestamp, pubKey, privKey, signature)
+  }
+  private fun SignedPreKeyRecord_Deserialize(serialized: ByteArray) : Long {
+    return Native.SignedPreKeyRecord_Deserialize(serialized)
+  }
+  private fun SignedPreKeyRecord_GetId(signedPreKeyRecord: Long) : Int {
+    return Native.SignedPreKeyRecord_GetId(signedPreKeyRecord)
+  }
+  private fun SignedPreKeyRecord_GetPrivateKey(signedPreKeyRecord: Long) : Long {
+    return Native.SignedPreKeyRecord_GetPrivateKey(signedPreKeyRecord)
+  }
+  private fun SignedPreKeyRecord_GetPublicKey(signedPreKeyRecord: Long) : Long {
+    return Native.SignedPreKeyRecord_GetPublicKey(signedPreKeyRecord)
+  }
+  private fun SignedPreKeyRecord_Serialize(signedPreKeyRecord: Long) : ByteArray {
+    return Native.SignedPreKeyRecord_GetSerialized(signedPreKeyRecord)
+  }
+  private fun SignedPreKeyRecord_GetSignature(signedPreKeyRecord: Long) : ByteArray {
+    return Native.SignedPreKeyRecord_GetSignature(signedPreKeyRecord)
+  }
+  private fun SignedPreKeyRecord_GetTimestamp(signedPreKeyRecord: Long) : Long {
+    return Native.SignedPreKeyRecord_GetTimestamp(signedPreKeyRecord)
+  }
+
+  private fun KyberPreKeyRecord_New(id: Int, timestamp: Long, keyPair: Long, signature: ByteArray) : Long {
+    return Native.KyberPreKeyRecord_New(id, timestamp, keyPair, signature)
+  }
+  private fun KyberPreKeyRecord_Serialize(kyberPreKeyRecord: Long) : ByteArray {
+    return Native.KyberPreKeyRecord_GetSerialized(kyberPreKeyRecord)
+  }
+  private fun KyberPreKeyRecord_Deserialize(serialized: ByteArray) : Long {
+    return Native.KyberPreKeyRecord_Deserialize(serialized)
+  }
+  private fun KyberPreKeyRecord_GetId(kyberPreKeyRecord: Long) : Int {
+    return Native.KyberPreKeyRecord_GetId(kyberPreKeyRecord)
+  }
+  private fun KyberPreKeyRecord_GetKeyPair(kyberPreKeyRecord: Long) : Long {
+    return Native.KyberPreKeyRecord_GetKeyPair(kyberPreKeyRecord)
+  }
+  private fun KyberPreKeyRecord_GetPublicKey(kyberPreKeyRecord: Long) : Long {
+    return Native.KyberPreKeyRecord_GetPublicKey(kyberPreKeyRecord)
+  }
+  private fun KyberPreKeyRecord_GetSecretKey(kyberPreKeyRecord: Long) : Long {
+    return Native.KyberPreKeyRecord_GetSecretKey(kyberPreKeyRecord)
+  }
+  private fun KyberPreKeyRecord_GetSignature(kyberPreKeyRecord: Long) : ByteArray {
+    return Native.KyberPreKeyRecord_GetSignature(kyberPreKeyRecord)
+  }
+  private fun KyberPreKeyRecord_GetTimestamp(kyberPreKeyRecord: Long) : Long {
+    return Native.KyberPreKeyRecord_GetTimestamp(kyberPreKeyRecord)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -128,5 +128,431 @@ export class PublicKey {
 	}
 }
 
+export class KEMPublicKey {
+	readonly _nativeHandle: Native.KyberPublicKey;
+
+	private constructor(handle: Native.KyberPublicKey) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(handle: Native.KyberPublicKey): KEMPublicKey {
+		return new KEMPublicKey(handle);
+	}
+
+	static deserialize(buf: Uint8Array): KEMPublicKey {
+		return new KEMPublicKey(
+			ExpoLibsignalClientModule.KyberPublicKey_Deserialize(buf)
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.KyberPublicKey_Serialize(
+			this._nativeHandle
+		);
+	}
+}
+
+export class KEMSecretKey {
+	readonly _nativeHandle: Native.KyberSecretKey;
+
+	private constructor(handle: Native.KyberSecretKey) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(handle: Native.KyberSecretKey): KEMSecretKey {
+		return new KEMSecretKey(handle);
+	}
+
+	static deserialize(buf: Uint8Array): KEMSecretKey {
+		return new KEMSecretKey(
+			ExpoLibsignalClientModule.KyberSecretKey_Deserialize(buf)
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.KyberSecretKey_Serialize(
+			this._nativeHandle
+		);
+	}
+}
+
+export class KEMKeyPair {
+	readonly _nativeHandle: Native.KyberKeyPair;
+
+	private constructor(handle: Native.KyberKeyPair) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(handle: Native.KyberKeyPair): KEMKeyPair {
+		return new KEMKeyPair(handle);
+	}
+
+	static generate(): KEMKeyPair {
+		return new KEMKeyPair(ExpoLibsignalClientModule.KyberKeyPair_Generate());
+	}
+
+	getPublicKey(): KEMPublicKey {
+		return KEMPublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.KyberKeyPair_GetPublicKey(this._nativeHandle)
+		);
+	}
+
+	getSecretKey(): KEMSecretKey {
+		return KEMSecretKey._fromNativeHandle(
+			ExpoLibsignalClientModule.KyberKeyPair_GetSecretKey(this._nativeHandle)
+		);
+	}
+}
+
+export class IdentityKeyPair {
+	readonly publicKey: PublicKey;
+	readonly privateKey: PrivateKey;
+
+	constructor(publicKey: PublicKey, privateKey: PrivateKey) {
+		this.publicKey = publicKey;
+		this.privateKey = privateKey;
+	}
+
+	static generate(): IdentityKeyPair {
+		const privateKey = PrivateKey.generate();
+		return new IdentityKeyPair(privateKey.getPublicKey(), privateKey);
+	}
+
+	static deserialize(buffer: Uint8Array): IdentityKeyPair {
+		const keys = ExpoLibsignalClientModule.IdentityKeyPair_Deserialize(buffer);
+		console.log({ keys });
+		return new IdentityKeyPair(
+			PublicKey._fromNativeHandle(keys[0]),
+			PrivateKey._fromNativeHandle(keys[1])
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.IdentityKeyPair_Serialize(
+			this.publicKey,
+			this.privateKey
+		);
+	}
+
+	signAlternateIdentity(other: PublicKey): Uint8Array {
+		return ExpoLibsignalClientModule.IdentityKeyPair_SignAlternateIdentity(
+			this.publicKey,
+			this.privateKey,
+			other
+		);
+	}
+}
+
+export class PreKeyBundle {
+	readonly _nativeHandle: Native.PreKeyBundle;
+
+	private constructor(handle: Native.PreKeyBundle) {
+		this._nativeHandle = handle;
+	}
+
+	// we are passing some arguments in array and receiving them as pairs for reducing the number of parameters to >= 8. we can clean it up further by putting it in a Record class whih is expo's max limit due to the limitations of generics in both Swift and Kotlin because this component must be implemented separately for each.
+	static new(
+		registration_id: number,
+		device_id: number,
+		prekey_id: number,
+		prekey: PublicKey,
+		signed_prekey_id: number,
+		signed_prekey: PublicKey,
+		signed_prekey_signature: Uint8Array,
+		identity_key: PublicKey,
+		kyber_prekey_id?: number,
+		kyber_prekey?: KEMPublicKey,
+		kyber_prekey_signature?: Uint8Array
+	): PreKeyBundle {
+		return new PreKeyBundle(
+			ExpoLibsignalClientModule.PreKeyBundle_New(
+				[registration_id, device_id],
+				prekey_id,
+				prekey != null ? prekey._nativeHandle : null,
+				signed_prekey_id,
+				[signed_prekey._nativeHandle, signed_prekey_signature],
+				identity_key._nativeHandle,
+				kyber_prekey_id ?? null,
+				[
+					kyber_prekey?._nativeHandle ?? null,
+					kyber_prekey_signature ?? new Uint8Array(0),
+				]
+			)
+		);
+	}
+
+	deviceId(): number {
+		return ExpoLibsignalClientModule.PreKeyBundle_GetDeviceId(
+			this._nativeHandle
+		);
+	}
+	identityKey(): PublicKey {
+		return PublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.PreKeyBundle_GetIdentityKey(this._nativeHandle)
+		);
+	}
+	preKeyId(): number | null {
+		return ExpoLibsignalClientModule.PreKeyBundle_GetPreKeyId(
+			this._nativeHandle
+		);
+	}
+	preKeyPublic(): PublicKey | null {
+		const handle = ExpoLibsignalClientModule.PreKeyBundle_GetPreKeyPublic(
+			this._nativeHandle
+		);
+
+		if (handle == null) {
+			return null;
+		}
+		return PublicKey._fromNativeHandle(handle);
+	}
+	registrationId(): number {
+		return ExpoLibsignalClientModule.PreKeyBundle_GetRegistrationId(
+			this._nativeHandle
+		);
+	}
+	signedPreKeyId(): number {
+		return ExpoLibsignalClientModule.PreKeyBundle_GetSignedPreKeyId(
+			this._nativeHandle
+		);
+	}
+	signedPreKeyPublic(): PublicKey {
+		return PublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.PreKeyBundle_GetSignedPreKeyPublic(
+				this._nativeHandle
+			)
+		);
+	}
+	signedPreKeySignature(): Uint8Array {
+		return ExpoLibsignalClientModule.PreKeyBundle_GetSignedPreKeySignature(
+			this
+		);
+	}
+
+	kyberPreKeyId(): number | null {
+		return ExpoLibsignalClientModule.PreKeyBundle_GetKyberPreKeyId(
+			this._nativeHandle
+		);
+	}
+
+	kyberPreKeyPublic(): KEMPublicKey | null {
+		const handle = ExpoLibsignalClientModule.PreKeyBundle_GetKyberPreKeyPublic(
+			this._nativeHandle
+		);
+		return handle == null ? null : KEMPublicKey._fromNativeHandle(handle);
+	}
+
+	kyberPreKeySignature(): Uint8Array | null {
+		const buf = ExpoLibsignalClientModule.PreKeyBundle_GetKyberPreKeySignature(
+			this._nativeHandle
+		);
+		return buf.length === 0 ? null : buf;
+	}
+}
+
+export class PreKeyRecord {
+	readonly _nativeHandle: Native.PreKeyRecord;
+
+	private constructor(handle: Native.PreKeyRecord) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(nativeHandle: Native.PreKeyRecord): PreKeyRecord {
+		return new PreKeyRecord(nativeHandle);
+	}
+
+	static new(id: number, pubKey: PublicKey, privKey: PrivateKey): PreKeyRecord {
+		return new PreKeyRecord(
+			ExpoLibsignalClientModule.PreKeyRecord_New(id, pubKey, privKey)
+		);
+	}
+
+	static deserialize(buffer: Uint8Array): PreKeyRecord {
+		return new PreKeyRecord(
+			ExpoLibsignalClientModule.PreKeyRecord_Deserialize(buffer)
+		);
+	}
+
+	id(): number {
+		return ExpoLibsignalClientModule.PreKeyRecord_GetId(this._nativeHandle);
+	}
+
+	privateKey(): PrivateKey {
+		return PrivateKey._fromNativeHandle(
+			ExpoLibsignalClientModule.PreKeyRecord_GetPrivateKey(this._nativeHandle)
+		);
+	}
+
+	publicKey(): PublicKey {
+		return PublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.PreKeyRecord_GetPublicKey(this._nativeHandle)
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.PreKeyRecord_Serialize(this._nativeHandle);
+	}
+}
+
+export class SignedPreKeyRecord {
+	readonly _nativeHandle: Native.SignedPreKeyRecord;
+
+	private constructor(handle: Native.SignedPreKeyRecord) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(
+		nativeHandle: Native.SignedPreKeyRecord
+	): SignedPreKeyRecord {
+		return new SignedPreKeyRecord(nativeHandle);
+	}
+
+	static new(
+		id: number,
+		timestamp: number,
+		pubKey: PublicKey,
+		privKey: PrivateKey,
+		signature: Uint8Array
+	): SignedPreKeyRecord {
+		return new SignedPreKeyRecord(
+			ExpoLibsignalClientModule.SignedPreKeyRecord_New(
+				id,
+				timestamp,
+				pubKey,
+				privKey,
+				signature
+			)
+		);
+	}
+
+	static deserialize(buffer: Uint8Array): SignedPreKeyRecord {
+		return new SignedPreKeyRecord(
+			ExpoLibsignalClientModule.SignedPreKeyRecord_Deserialize(buffer)
+		);
+	}
+
+	id(): number {
+		return ExpoLibsignalClientModule.SignedPreKeyRecord_GetId(
+			this._nativeHandle
+		);
+	}
+
+	privateKey(): PrivateKey {
+		return PrivateKey._fromNativeHandle(
+			ExpoLibsignalClientModule.SignedPreKeyRecord_GetPrivateKey(
+				this._nativeHandle
+			)
+		);
+	}
+
+	publicKey(): PublicKey {
+		return PublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.SignedPreKeyRecord_GetPublicKey(
+				this._nativeHandle
+			)
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.SignedPreKeyRecord_Serialize(
+			this._nativeHandle
+		);
+	}
+
+	signature(): Uint8Array {
+		return ExpoLibsignalClientModule.SignedPreKeyRecord_GetSignature(
+			this._nativeHandle
+		);
+	}
+
+	timestamp(): number {
+		return ExpoLibsignalClientModule.SignedPreKeyRecord_GetTimestamp(
+			this._nativeHandle
+		);
+	}
+}
+
+export class KyberPreKeyRecord {
+	readonly _nativeHandle: Native.KyberPreKeyRecord;
+
+	private constructor(handle: Native.KyberPreKeyRecord) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(
+		nativeHandle: Native.KyberPreKeyRecord
+	): KyberPreKeyRecord {
+		return new KyberPreKeyRecord(nativeHandle);
+	}
+
+	static new(
+		id: number,
+		timestamp: number,
+		keyPair: KEMKeyPair,
+		signature: Uint8Array
+	): KyberPreKeyRecord {
+		return new KyberPreKeyRecord(
+			ExpoLibsignalClientModule.KyberPreKeyRecord_New(
+				id,
+				timestamp,
+				keyPair,
+				signature
+			)
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.KyberPreKeyRecord_Serialize(
+			this._nativeHandle
+		);
+	}
+
+	static deserialize(buffer: Uint8Array): KyberPreKeyRecord {
+		return new KyberPreKeyRecord(
+			ExpoLibsignalClientModule.KyberPreKeyRecord_Deserialize(buffer)
+		);
+	}
+
+	id(): number {
+		return ExpoLibsignalClientModule.KyberPreKeyRecord_GetId(
+			this._nativeHandle
+		);
+	}
+
+	keyPair(): KEMKeyPair {
+		return KEMKeyPair._fromNativeHandle(
+			ExpoLibsignalClientModule.KyberPreKeyRecord_GetKeyPair(this._nativeHandle)
+		);
+	}
+
+	publicKey(): KEMPublicKey {
+		return KEMPublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.KyberPreKeyRecord_GetPublicKey(
+				this._nativeHandle
+			)
+		);
+	}
+
+	secretKey(): KEMSecretKey {
+		return KEMSecretKey._fromNativeHandle(
+			ExpoLibsignalClientModule.KyberPreKeyRecord_GetSecretKey(
+				this._nativeHandle
+			)
+		);
+	}
+
+	signature(): Uint8Array {
+		return ExpoLibsignalClientModule.KyberPreKeyRecord_GetSignature(
+			this._nativeHandle
+		);
+	}
+
+	timestamp(): number {
+		return ExpoLibsignalClientModule.KyberPreKeyRecord_GetTimestamp(
+			this._nativeHandle
+		);
+	}
+}
+
 //export the types here
 // export {};
