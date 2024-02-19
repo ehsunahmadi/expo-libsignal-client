1a1a013159b3854db7049527d833e86713ccd1e1
ehsun <ehsun.ahmadi@gmail.com>
2024-02-19 21:50:30 +0330

    hkdf, privateKey, publicKey modules added

--- android/src/main/java/expo/modules/libsignalclient/ExpoLibsignalClientModule.kt
+++ android/src/main/java/expo/modules/libsignalclient/ExpoLibsignalClientModule.kt
@@ -8,20 +8,62 @@ class ExpoLibsignalClientModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("ExpoLibsignalClient")
 
+    Function("HKDF_DeriveSecrets", this@ExpoLibsignalClientModule::HKDF_DeriveSecrets)
+
     Function("PrivateKey_Generate", this@ExpoLibsignalClientModule::PrivateKey_Generate)
     Function("PrivateKey_Deserialize", this@ExpoLibsignalClientModule::PrivateKey_Deserialize)
     Function("PrivateKey_Serialize", this@ExpoLibsignalClientModule::PrivateKey_Serialize)
+    Function("PrivateKey_Sign", this@ExpoLibsignalClientModule::PrivateKey_Sign)
+    Function("PrivateKey_Agree", this@ExpoLibsignalClientModule::PrivateKey_Agree)
+    Function("PrivateKey_GetPublicKey", this@ExpoLibsignalClientModule::PrivateKey_GetPublicKey)
+
+    Function("PublicKey_Deserialize", this@ExpoLibsignalClientModule::PublicKey_Deserialize)
+    Function("PublicKey_Compare", this@ExpoLibsignalClientModule::PublicKey_Compare)
+    Function("PublicKey_Serialize", this@ExpoLibsignalClientModule::PublicKey_Serialize)
+    Function("PublicKey_GetPublicKeyBytes", this@ExpoLibsignalClientModule::PublicKey_GetPublicKeyBytes)
+    Function("PublicKey_Verify", this@ExpoLibsignalClientModule::PublicKey_Verify)
+    Function("IdentityKey_VerifyAlternateIdentity", this@ExpoLibsignalClientModule::IdentityKey_VerifyAlternateIdentity)
+  }
+
+  private fun HKDF_DeriveSecrets(outputLength: Int, ikm: ByteArray, label: ByteArray | null, salt: ByteArray | null) : ByteArray {
+    return Native.HKDF_DeriveSecrets(outputLength, ikm, label, salt)
   }
 
   private fun PrivateKey_Generate() : Long  {
     return Native.ECPrivateKey_Generate()
   }
-
   private fun PrivateKey_Deserialize(serialized: ByteArray) : Long {
     return Native.ECPrivateKey_Deserialize(serialized)
   }
-
   private fun PrivateKey_Serialize(privateKey: Long) : ByteArray {
     return Native.ECPrivateKey_Serialize(privateKey)
+  }
+  private fun PrivateKey_Sign(privateKey: Long, message: ByteArray) : ByteArray {
+    return Native.ECPrivateKey_Sign(privateKey, message)
+  }
+  private fun PrivateKey_Agree(privateKey: Long, publicKey: Long) : ByteArray {
+    return Native.ECPrivateKey_Agree(privateKey, publicKey)
+  }
+  private fun PrivateKey_GetPublicKey(privateKey: Long) : Long {
+    return Native.ECPrivateKey_GetPublicKey(privateKey)
+  }
+
+  private fun PublicKey_Deserialize(serialized: ByteArray) : Long {
+    return Native.ECPublicKey_Deserialize(serialized, 0)
+  }
+  private fun PublicKey_Compare(publicKey1: Long, publicKey2: Long) : Int {
+    return Native.ECPublicKey_Compare(publicKey1, publicKey2)
+  }
+  private fun PublicKey_Serialize(publicKey: Long) : ByteArray {
+    return Native.ECPublicKey_Serialize(publicKey)
+  }
+  private fun PublicKey_GetPublicKeyBytes(publicKey: Long) : ByteArray {
+    return Native.ECPublicKey_GetPublicKeyBytes(publicKey)
+  }
+  private fun PublicKey_Verify(publicKey: Long, message: ByteArray, signature: ByteArray) : Boolean {
+    return Native.ECPublicKey_Verify(publicKey, message, signature)
+  }
+  private fun IdentityKey_VerifyAlternateIdentity(identityKey: Long, publicKey: Long, signature: ByteArray) : Boolean {
+    return Native.IdentityKey_VerifyAlternateIdentity(identityKey, publicKey, signature)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,40 @@
 import ExpoLibsignalClientModule from './ExpoLibsignalClientModule';
 import Native from './Native';
+
+export class HKDF {
+	/**
+	 * @deprecated Use the top-level 'hkdf' function for standard HKDF behavior
+	 */
+	static new(version: number): HKDF {
+		if (version !== 3) {
+			throw new Error('HKDF versions other than 3 are no longer supported');
+		}
+		return new HKDF();
+	}
+
+	deriveSecrets(
+		outputLength: number,
+		keyMaterial: Uint8Array,
+		label: Uint8Array,
+		salt: Uint8Array | null
+	): Uint8Array {
+		return hkdf(outputLength, keyMaterial, label, salt);
+	}
+}
+
+export function hkdf(
+	outputLength: number,
+	keyMaterial: Uint8Array,
+	label: Uint8Array,
+	salt: Uint8Array | null
+): Uint8Array {
+	return ExpoLibsignalClientModule.HKDF_DeriveSecrets(
+		outputLength,
+		keyMaterial,
+		label,
+		salt
+	);
+}
 export class PrivateKey {
 	readonly _nativeHandle: Native.PrivateKey;
 
@@ -25,17 +60,72 @@ export class PrivateKey {
 		return ExpoLibsignalClientModule.PrivateKey_Serialize(this._nativeHandle);
 	}
 
-	// sign(msg: Buffer): Buffer {
-	// 	return ExpoLibsignalClientModule.PrivateKey_Sign(this, msg);
-	// }
+	sign(msg: Uint8Array): Uint8Array {
+		return ExpoLibsignalClientModule.PrivateKey_Sign(this._nativeHandle, msg);
+	}
 
-	// agree(other_key: PublicKey): Buffer {
-	//   return Native.PrivateKey_Agree(this, other_key);
-	// }
+	agree(other_key: PublicKey): Uint8Array {
+		return ExpoLibsignalClientModule.PrivateKey_Agree(
+			this._nativeHandle,
+			other_key
+		);
+	}
 
-	// getPublicKey(): PublicKey {
-	//   return PublicKey._fromNativeHandle(Native.PrivateKey_GetPublicKey(this));
-	// }
+	getPublicKey(): PublicKey {
+		return PublicKey._fromNativeHandle(
+			ExpoLibsignalClientModule.PrivateKey_GetPublicKey(this._nativeHandle)
+		);
+	}
+}
+
+export class PublicKey {
+	readonly _nativeHandle: Native.PublicKey;
+
+	private constructor(handle: Native.PublicKey) {
+		this._nativeHandle = handle;
+	}
+
+	static _fromNativeHandle(handle: Native.PublicKey): PublicKey {
+		return new PublicKey(handle);
+	}
+
+	static deserialize(buf: Uint8Array): PublicKey {
+		return new PublicKey(ExpoLibsignalClientModule.PublicKey_Deserialize(buf));
+	}
+
+	/// Returns -1, 0, or 1
+	compare(other: PublicKey): number {
+		return ExpoLibsignalClientModule.PublicKey_Compare(
+			this._nativeHandle,
+			other
+		);
+	}
+
+	serialize(): Uint8Array {
+		return ExpoLibsignalClientModule.PublicKey_Serialize(this._nativeHandle);
+	}
+
+	getPublicKeyBytes(): Uint8Array {
+		return ExpoLibsignalClientModule.PublicKey_GetPublicKeyBytes(
+			this._nativeHandle
+		);
+	}
+
+	verify(msg: Uint8Array, sig: Uint8Array): boolean {
+		return ExpoLibsignalClientModule.PublicKey_Verify(
+			this._nativeHandle,
+			msg,
+			sig
+		);
+	}
+
+	verifyAlternateIdentity(other: PublicKey, signature: Uint8Array): boolean {
+		return ExpoLibsignalClientModule.IdentityKey_VerifyAlternateIdentity(
+			this._nativeHandle,
+			other,
+			signature
+		);
+	}
 }
 
 //export the types here
